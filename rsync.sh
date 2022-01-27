ce8342735955e6ce8ce17da5780919f42a35ea20
sieunleeeee <93171719+sieunleeeee@users.noreply.github.com>
2022-01-27 15:10:52 +0900

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- rsync -avrz --delete /var/jenkins_home/workspace/tset/target/hello-world-maven-0.1.0-shaded.war 15.164.230.227::backup
+rsync -avrz --delete /var/jenkins_home/workspace/tset/target/*.war 15.164.230.227::backup
